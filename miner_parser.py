e217d84cdaafa1239dda1c7021892668e04f4a2d
YAMAGUCHI, Rei <ray@ymgch.org>
2018-01-14 07:21:03 +0000

    first version

---
+++ miner_parser.py
@@ -0,0 +1,25 @@
+import re
+from dateutil.parser import parse
+
+
+def parse_miner(logger, line):
+    syslog_header, message = line.split(']:', 1)
+
+    # hash rate
+    matches = re.search(r'accepted: .+?\),\s([0-9\.]+)\s(\S)(hash|H)/s\s\((yes|yay)!+\)', message)
+    if not matches:
+        return None
+
+    raw_value = float(matches.group(1))
+    si_unit = matches.group(2)
+    if si_unit.upper() == 'G':
+        value = raw_value * 1000 * 1000 * 1000
+    elif si_unit.upper() == 'M':
+        value = raw_value * 1000 * 1000
+    elif si_unit.upper() == 'K':
+        value = raw_value * 1000
+
+    # timestamp
+    time_string = " ".join(syslog_header.split(" ", 3)[0:3])
+    timestamp = int(parse(time_string).strftime('%s'))
+    return ("miner.hashrate", timestamp, value, {'metric_type': 'gauge', 'unit': 'hash/s'})
---
+++ test_miner_parser.py
@@ -0,0 +1,35 @@
+import miner_parser
+import unittest
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+class TestMinerParser(unittest.TestCase):
+    def test_parse_miner(self):
+        # Set up the test input and expected output
+        testdata = [
+            {"test_input": "Jan 14 13:55:15 server1 cpuminer[2355]: accepted: 94/94 (100.00%), 1.15 khash/s (yay!!!)",
+             "expected": (
+                "miner.hashrate",
+                1515938115,
+                1150.0,
+                {"metric_type": "gauge",
+                 "unit": "hash/s"})},
+            {"test_input": "Jan 14 13:55:15 server1 cpuminer[2355]: accepted: 2118/2126 (99.62%), 1.15 khash/s (booooo)",
+             "expected": None},
+            {"test_input": "Jan 14 13:55:15 server1 ccminer[4345]: accepted: 27/27 (diff 0.443), 13.91 MH/s (yes!)",
+             "expected": (
+                "miner.hashrate",
+                1515938115,
+                13910000.0,
+                {"metric_type": "gauge",
+                 "unit": "hash/s"})},
+            {"test_input": "Jan 14 13:55:15 server1 ccminer[589]: accepted: 1591/1592 (diff 0.030), 13.86 MH/s (booooo)",
+             "expected": None},
+            {"test_input": "Jan 14 13:36:58 botia systemd[1]: Started Start CPUMiner.",
+             "expected": None},
+        ]
+        for data in testdata:
+            actual = miner_parser.parse_miner(logging, data['test_input'])
+            expected = data['expected']
+            self.assertEqual(expected, actual, "%s != %s" % (expected, actual))
